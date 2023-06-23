991a3c86f1536f73a25e64e4a4aded6f556c6c7a
Anthony Bernardo Kamers <anthony.kamers@ufsc.br>
2023-06-23 13:03:47 -0300

    Modificação para fazer os dois testes de aceitação do roteiro

--- src/test/java/acceptanceTests/cucumberSteps/CadastrarProdutoTest.java
+++ src/test/java/acceptanceTests/cucumberSteps/CadastrarProdutoTest.java
@@ -1,17 +1,40 @@
 package acceptanceTests.cucumberSteps;
 
+import io.cucumber.java.ParameterType;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.example.interfaces.IMercadoLeilao;
+import org.example.modelo.MercadoLeilao;
 import org.junit.Assert;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class CadastrarProdutoTest {
     public String nomeProduto = "";
     public String descricaoProduto = "";
     public Double valorMinimoProduto = 0.0;
     public String cpfLeiloador = "";
+    public Date dataLimiteProduto;
     public boolean cadastro = false;
+    public boolean expectedError = false;
+    public static IMercadoLeilao mercado = new MercadoLeilao();
+
+    // Background
+    @Given("O cadastro do usuario Ernani Cesar foi realizado")
+    public void oCadastroDoUsuarioErnaniCesarFoiRealizado() throws Exception {
+        String nome = "Ernani Cesar";
+        String endereco = "Camus Universitário";
+        String CPF = "111.111.111-11";
+        String email = "ernani.santos@posgrad.ufsc.br";
+        mercado.cadastrarUsuario(nome, endereco, email, CPF);
+    }
+
+    // Scenario: Cadastrar Produto com Sucesso
 
     @Given("O nome do produto {string}")
     public void o_nome_do_produto(String string) {
@@ -33,10 +56,21 @@ public class CadastrarProdutoTest {
         this.cpfLeiloador = string;
     }
 
+    @And("A data limite do produto no leilao eh {string}")
+    public void aDataLimiteDoProdutoNoLeilaoEh(String dateString) {
+        LocalDate teste = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        this.dataLimiteProduto = Date.from(teste.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
     @When("O produto eh cadastrado")
     public void o_produto_eh_cadastrado() {
-        System.out.println("vou cadastrar o produto agora");
-        cadastro = true;
+        try {
+            mercado.cadastrarProduto(nomeProduto, descricaoProduto, valorMinimoProduto, cpfLeiloador, dataLimiteProduto);
+            cadastro = true;
+            mercado = new MercadoLeilao();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Then("O produto eh incluido com sucesso")
@@ -44,8 +78,38 @@ public class CadastrarProdutoTest {
         Assert.assertTrue(cadastro);
     }
 
+    // Scenario:
     @Given("o nome eh {string}")
     public void oNomeEh(String arg0) {
         System.out.println(arg0);
+    }
+
+    @Given("O produto Bola de futebol")
+    public void oProdutoBolaDeFutebol() {
+        try {
+            String nome = "Bola de futebol";
+            String descricao = "";
+            Double valorMinimo = 1.2;
+            String cpf = "111.111.111-11";
+            Date date = new Date();
+            mercado.cadastrarProduto(nome, descricao, valorMinimo, cpf, date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @When("Jah existe um produto com o mesmo nome")
+    public void jahExisteUmProdutoComOMesmoNome() {
+        try {
+            mercado.cadastrarProduto(nomeProduto, descricaoProduto, valorMinimoProduto, cpfLeiloador, dataLimiteProduto);
+        } catch (Exception e) {
+            if (e.getMessage().equals("O produto ja existe ou o leiloador nao esta cadastrado."))
+                expectedError = true;
+        }
+    }
+
+    @Then("Eh recebida uma mensagem informando que jah existe um produto cadastrado")
+    public void ehRecebidaMensagemJahExisteProduto() {
+        Assert.assertTrue(expectedError);
     }
 }
--- src/test/java/features/CadastrarProduto_Test.feature
+++ src/test/java/features/CadastrarProduto_Test.feature
@@ -2,20 +2,23 @@ Feature: Cadastrar Produto
   Cadastrar produtos
 
   Background:
-    Given O nome de usuario "Joao da silva"
-    And o endereco "Campus Universitario"
-    And e o CPF "111.111.111-11"
-    And e o e-mail "teste@teste.com.br"
-    When O usuario eh cadastrado
-    Then O usuario eh incluido com sucesso
+    Given O cadastro do usuario Ernani Cesar foi realizado
 
   Scenario: Cadastrar Produto com Sucesso
     Given O nome do produto "Bola de futebol"
     And   A descricao do produto "Oficial Fifa"
     And   Valor minimo eh 1.2
     And   CPF do leiloador eh "111.111.111-11"
+    And   A data limite do produto no leilao eh "24/06/2023"
     When  O produto eh cadastrado
     Then  O produto eh incluido com sucesso
 
-  Scenario: Teste
-    Given o nome eh "teste"
+  Scenario: Cadastrar produto com mesmo nome gera erro
+    Given O produto Bola de futebol
+    Given O nome do produto "Bola de futebol"
+    And   A descricao do produto "Oficial Fifa"
+    And   Valor minimo eh 1.2
+    And   CPF do leiloador eh "111.111.111-11"
+    And   A data limite do produto no leilao eh "24/06/2023"
+    When  Jah existe um produto com o mesmo nome
+    Then  Eh recebida uma mensagem informando que jah existe um produto cadastrado
